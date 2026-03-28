3b4f71b8e40f57a1ca688b0fa24839a3f2972a21
bradley schwartz <the97bradley@gmail.com>
2026-03-27 21:35:37 -0600

    Move embedded map center a bit further north

--- src/components/EventSnapshotMap.tsx
+++ src/components/EventSnapshotMap.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 const PUBLIC_BOUNDARY_MAP_URL =
-  "https://www.google.com/maps/d/viewer?mid=1kMz1441dwvdGMiIVmdIYmaqobOUzFQ4&ll=39.7214%2C-104.9929&z=15";
+  "https://www.google.com/maps/d/viewer?mid=1kMz1441dwvdGMiIVmdIYmaqobOUzFQ4&ll=39.7219%2C-104.9929&z=15";
 
 const EMBED_BOUNDARY_MAP_URL =
-  "https://www.google.com/maps/d/embed?mid=1kMz1441dwvdGMiIVmdIYmaqobOUzFQ4&ll=39.7214%2C-104.9929&z=15";
+  "https://www.google.com/maps/d/embed?mid=1kMz1441dwvdGMiIVmdIYmaqobOUzFQ4&ll=39.7219%2C-104.9929&z=15";
 
 export default function EventSnapshotMap() {
   return (
